201e1803d1d2668f4b7f86007ab7154545505faf
kamishubaizumo <84670496+kamishubaizumo@users.noreply.github.com>
2022-12-27 01:33:21 +0000

    どうしても呼び出せないエラーメッセージ

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+#deleteになってたが・・・？
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    book_id = @book.user.id
+    login_user_id = current_user.id
+    if(book_id != login_user_id)
+      redirect_to books_path
+    end
+  end
+end
