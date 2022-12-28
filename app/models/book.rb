f4ba363e410c85b16381d7e0c0b20dfe13556c98
kamishubaizumo <84670496+kamishubaizumo@users.noreply.github.com>
2022-12-28 05:06:17 +0000

    イイネ、コメント機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,21 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+
+
+  #favorite
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+#exists?　・・・　引数で渡されたuser.idがfavoritesテーブルに存在、exists?　するか調べる。
+
+
+
+
+
+
 end
